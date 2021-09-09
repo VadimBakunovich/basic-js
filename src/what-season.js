c46a38cacd920bb49cddb3f9c9e6a64bb58d37d8
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-09-09 20:57:33 +0300

    feat: solve the what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (!(date + '').includes('GMT')) throw new Error('Invalid date!');
+  if ((date + '') == new Date()) throw new Error('Invalid date!');
+  switch (date.getMonth()) {
+    case 11: return 'winter';
+    case 0: return 'winter';
+    case 1: return 'winter';
+    case 2: return 'spring';
+    case 3: return 'spring';
+    case 4: return 'spring';
+    case 5: return 'summer';
+    case 6: return 'summer';
+    case 7: return 'summer';
+    case 8: return 'fall';
+    case 9: return 'fall';
+    case 10: return 'fall';
+    default: break;
+  }
 }
