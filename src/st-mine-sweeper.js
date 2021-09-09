dcf19feea7d10b398c8dc474a0a2c9c6573c77ed
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-09-09 12:17:20 +0300

    feat: solve the st-mine-sweeper task

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,22 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (m, res = []) {
+  for (let i = 0; i < m.length; i++) {
+    let arr = [];
+    for (let j = 0; j < m[i].length; j++) {
+      let count = 0;
+      if (i > 0) m[i-1][j] ? count++ : count; // up
+      if (i > 0 && j < m[i].length - 1) m[i-1][j+1] ? count++ : count; // up-right
+      if (j < m[i].length - 1) m[i][j+1] ? count++ : count; // right
+      if (i < m.length - 1 && j < m[i].length - 1) m[i+1][j+1] ? count++ : count; // down-right
+      if (i < m.length - 1) m[i+1][j] ? count++ : count; // down
+      if (i < m.length - 1 && j > 0) m[i+1][j-1] ? count++ : count; // down-left
+      if (j > 0) m[i][j-1] ? count++ : count; // left
+      if (i > 0 && j > 0) m[i-1][j-1] ? count++ : count; // up-left
+      arr.push(count);
+    }
+    res.push(arr);
+  }
+  return res;
 }
