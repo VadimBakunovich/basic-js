0ed5fadba3ffe673ad6d4d3972158d6f178b4882
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-09-09 15:01:59 +0300

    feat: solve the transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!');
+  const diN = '--discard-next';
+  const diP = '--discard-prev';
+  const doN = '--double-next';
+  const doP = '--double-prev';
+  let res = [...arr];
+  let i = 0;
+  for (let i = 0; i < res.length;) {
+    if ((res[i] === diP || res[i] === doP) && i === 0) res[i] = 0;
+    else if ((res[i] === diN || res[i] === doN) && i === res.length - 1) res[i] = 0;
+    else if (res[i] === diN && i < res.length - 1) {res[i] = 0; res[i + 1] = 0; i++;}
+    else if (res[i] === diP && i > 0) {res[i - 1] = 0; res[i] = 0; i++}
+    else if (res[i] === doN && i < res.length - 1) {
+      res[i] = res[i + 1];
+      i++;
+    } else if (res[i] === doP && i > 0) {
+        res[i] = res[i - 1];
+        i++;
+      }
+    i++;
+  }
+  return res.filter(i => i !== 0);
 }
